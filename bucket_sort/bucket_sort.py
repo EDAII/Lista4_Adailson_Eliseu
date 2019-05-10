48aea4c78be5a934beda20843ad8541b1aa9eda1
Adailson <adailson2@gmail.com>
2019-05-10 16:04:12 -0300

    Bucketsort com radixsort em sub-buckets

--- bucket_sort/bucket_sort.py
+++ bucket_sort/bucket_sort.py
@@ -1,3 +1,35 @@
+def radix_sort(vector, base=10):
+    if vector == []:
+        return
+ 
+    def key_factory(digit, base):
+        def key(vector, index):
+            return ((vector[index]//(base**digit)) % base)
+        return key
+    largest = max(vector)
+    exp = 0
+    while base**exp <= largest:
+        vector = counting_sort(vector, base - 1, key_factory(exp, base))
+        exp = exp + 1
+    return vector
+ 
+def counting_sort(vector, largest, key):
+    c = [0]*(largest + 1)
+    for i in range(len(vector)):
+        c[key(vector, i)] = c[key(vector, i)] + 1
+ 
+    # Find the last index for each element
+    c[0] = c[0] - 1 # to decrement each element for zero-based indexing
+    for i in range(1, largest + 1):
+        c[i] = c[i] + c[i - 1]
+ 
+    result = [None]*len(vector)
+    for i in range(len(vector) - 1, -1, -1):
+        result[c[key(vector, i)]] = vector[i]
+        c[key(vector, i)] = c[key(vector, i)] - 1
+ 
+    return result
+
 def insertionSort(vector): 
 	for i in range(1, len(vector)): 
 		up = vector[i] 
@@ -18,12 +50,34 @@ def iterative_bucket_sort(vector):
 		str_vec = str(vector[j])
 		dif = len(str(max(vector))) - len(str(vector[j]))
 		str_vec = '0' * dif + str_vec
-		#print(str_vec)
 		sig = int(str_vec[0])
 		arr[sig].append(vector[j])
 	
 	for i in range(number_buckets): 
 		arr[i] = insertionSort(arr[i]) 
+		
+	k = 0
+	for i in range(number_buckets): 
+		for j in range(len(arr[i])): 
+			vector[k] = arr[i][j] 
+			k += 1
+	return vector
+
+def bucket_radix_sort(vector): 
+	arr = [] 
+	number_buckets = 10
+	for i in range(number_buckets): 
+		arr.append([]) 
+		
+	for j in  range(0, len(vector)):
+		str_vec = str(vector[j])
+		dif = len(str(max(vector))) - len(str(vector[j]))
+		str_vec = '0' * dif + str_vec
+		sig = int(str_vec[0])
+		arr[sig].append(vector[j])
+	
+	for i in range(number_buckets): 
+		arr[i] = radix_sort(arr[i]) 
 		
 	k = 0
 	for i in range(number_buckets): 
--- radix_sort/radix_sort.py
+++ radix_sort/radix_sort.py
@@ -1,36 +1,36 @@
-def radix_sort(alist, base=10):
-    if alist == []:
+def radix_sort(vector, base=10):
+    if vector == []:
         return
  
     def key_factory(digit, base):
-        def key(alist, index):
-            return ((alist[index]//(base**digit)) % base)
+        def key(vector, index):
+            return ((vector[index]//(base**digit)) % base)
         return key
-    largest = max(alist)
+    largest = max(vector)
     exp = 0
     while base**exp <= largest:
-        alist = counting_sort(alist, base - 1, key_factory(exp, base))
+        vector = counting_sort(vector, base - 1, key_factory(exp, base))
         exp = exp + 1
-    return alist
+    return vector
  
-def counting_sort(alist, largest, key):
+def counting_sort(vector, largest, key):
     c = [0]*(largest + 1)
-    for i in range(len(alist)):
-        c[key(alist, i)] = c[key(alist, i)] + 1
+    for i in range(len(vector)):
+        c[key(vector, i)] = c[key(vector, i)] + 1
  
     # Find the last index for each element
     c[0] = c[0] - 1 # to decrement each element for zero-based indexing
     for i in range(1, largest + 1):
         c[i] = c[i] + c[i - 1]
  
-    result = [None]*len(alist)
-    for i in range(len(alist) - 1, -1, -1):
-        result[c[key(alist, i)]] = alist[i]
-        c[key(alist, i)] = c[key(alist, i)] - 1
+    result = [None]*len(vector)
+    for i in range(len(vector) - 1, -1, -1):
+        result[c[key(vector, i)]] = vector[i]
+        c[key(vector, i)] = c[key(vector, i)] - 1
  
     return result
 
 
-alist = [12,23,4,5,5123,34,34543,543,53,434313,4,1,0]
-sorted_list = radix_sort(alist)
+vector = [12,23,4,5,5123,34,34543,543,53,434313,4,1,0]
+sorted_list = radix_sort(vector)
 print(sorted_list)
